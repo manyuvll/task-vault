d35d5754f276b9c2cd8ebb3a518b01ba345b6172
Emanuele Vella <manyuvella@gmail.com>
2023-09-27 23:33:55 +0900

    fix: fix padding in home scrollview parent

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
   scrollView: {
     gap: 10,
     paddingHorizontal: 30,
-    paddingBottom: 100,
+    paddingBottom: 140,
   },
   titleContainer: {
     flexDirection: "row",
